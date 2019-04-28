1f49e68f068fc321295f3498f37aa06bed018113
Lawrence Glenn <Lawrence.O.Glenn@gmail.com>
2019-04-27 21:53:15 -0700

    moved die factory to its own js file

---
+++ scripts/dice.js
@@ -0,0 +1,19 @@
+
+const Die = (sides) => {
+  const getSides = () => sides;
+  const roll = (times) => {
+    var total = 0;
+    for (var i = 0; i < times; i++) {
+    total += (Math.random() * sides) + 1;
+    }
+    return total;
+  }
+}
+
+const d4 = Die(4);
+const d6 = Die(6);
+const d8 = Die(8);
+const d10 = Die(10);
+const d12 = Die(12);
+const d20 = Die(20);
+const d100 = Die(100);
